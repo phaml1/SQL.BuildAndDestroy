46192bb94977b74ed4f832342592e30d99c10ebf
Loc <loc@Zipcoders-MacBook-Pro-3.local>
2023-11-26 23:32:53 -0500

    completed excercises 1-8

---
+++ answers/exercise8.sql
@@ -0,0 +1,9 @@
+INSERT INTO Students (StudentName,
+                      Address,
+                      City,
+                      PostalCode,
+                      Country)
+VALUES (Jane Doe,
+        57 Union St,
+        Glasgow, Scotland
+        G13RB);
